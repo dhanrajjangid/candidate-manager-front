7c535d471f72c22d2e21c353ddce693faede64c8
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2024-10-14 17:39:38 +0530

    url change

--- src/view/Chat/socket.jsx
+++ src/view/Chat/socket.jsx
@@ -1,6 +1,6 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://candidate-backend-mkcp.onrender.com';
+const URL = "https://candidate-backend-mkcp.onrender.com";
 
 export const socket = io(URL);
